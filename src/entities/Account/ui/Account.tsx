44570cb63f066a2859aa67c956edf87e5b2b8a57
Ildar <money_ild@mail.ru>
2024-05-29 01:58:13 +0300

    fixed tg-user-data-login

--- src/entities/Account/ui/Account.tsx
+++ src/entities/Account/ui/Account.tsx
@@ -157,7 +157,7 @@ export const Account = memo(() => {
         const button = document.createElement('script');
         button.async = true;
         button.src = 'https://telegram.org/js/telegram-widget.js?22';
-        button.setAttribute('data-telegram-login', 'blessrain_bot,en');
+        button.setAttribute('data-telegram-login', 'blessrain_bot');
         button.setAttribute('data-size', 'large');
         button.setAttribute('data-userpic', 'false');
         button.setAttribute('data-radius', '8');
